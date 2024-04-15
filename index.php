54272dcecf4f26827056dbe52eb5adc7d6aeaab1
Daniele206 <danieledeieso2004@gmail.com>
2024-04-15 16:17:10 +0200

    [add] style

--- index.php
+++ index.php
@@ -45,13 +45,15 @@ $hotels = [
 <head>
   <meta charset="UTF-8">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP-Hotel</title>
 </head>
-<body>
-  <div class="container d-flex flex-wrap">
+<body class="bg-primary my_heigt-100">
+  <h1 class="text-center fw-bold pt-5">Hotel</h1>
+  <div class="container d-flex flex-wrap align-items-center h-75">
     <?php foreach($hotels as $hotel): ?>
-      <div class="card" style="width: 18rem;">
+      <div class="card mx-5" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title"><?php echo $hotel['name'] ?></h5>
           <h6 class="card-subtitle mb-2 text-body-secondary"><?php echo $hotel['description'] ?></h6>
